9d3307739fc970d649a97c05df4598f5c0ce7e60
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-04-27 20:07:03 -0300

    update files

--- views/createuser.php
+++ views/createuser.php
@@ -1,5 +1,13 @@
 <div class="container mt-5">
-
-    <p>Hello world!</p>
+    <div class="row">
+        <div class=" col-md-12">
+            <div class="card">
+                <div class=" card-header">
+                    <h4>Adicionar usuário</h4>
+                    <a href="home" class=" btn btn-danger float-end"></a>Voltar a tela inicial
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 </div>
