5ad7f68544e571f553311aeb88a76ef31ed8804a
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-04-27 22:50:24 -0300

    update files

--- views/createuser.php
+++ views/createuser.php
@@ -1,10 +1,34 @@
-<div class="container mt-5">
+<div class="container g-3">
     <div class="row">
         <div class=" col-md-12">
             <div class="card">
                 <div class=" card-header">
-                    <h4>Adicionar usuário</h4>
-                    <a href="home" class=" btn btn-danger float-end"></a>Voltar a tela inicial
+                    <h4>Adicionar usuário
+                      <a href="home" class=" btn btn-danger float-end">Voltar</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <div class="mb-3">
+                            <label>Nome</label>
+                            <input class="form-control"type="text" name="nome" placeholder="Digite o nome do novo Usuário" required>
+                        </div>
+                        <div class="mb-3">
+                            <label>Email</label>
+                            <input class="form-control"type="text" name="email" placeholder="Digite o nome do novo Usuário" required>
+                        </div>
+                        <div class="mb-3">
+                            <label>Data de Nascimento</label>
+                            <input class="form-control"type="date" name="data_nascimento" placeholder="Digite o nome do novo Usuário" required>
+                        </div>
+                        <div class="mb-3">
+                            <label>Senha</label>
+                            <input class="form-control" type="password" name="nome" placeholder="Digite o nome do novo Usuário" required>
+                        </div>
+                        <div class="mb-3">
+                            <button class="btn btn-primary" type="submit" name="createuser">Enviar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
