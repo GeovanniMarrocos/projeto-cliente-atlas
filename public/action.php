b8617864db0e5bf64c7e55d795407e08294bdf20
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-04-29 15:04:33 -0300

    update files

--- public/action.php
+++ public/action.php
@@ -1,4 +1,5 @@
  <?php
+
 session_start();
 require_once("../vendor/autoload.php");
 require_once("../config/database.php");
@@ -7,11 +8,13 @@ if (isset($_POST['createuser']))  {
     $nome = mysqli_real_escape_string($connection, trim($_POST['nome']));
     $email = mysqli_real_escape_string($connection, trim($_POST['email']));
     $data_nascimento = mysqli_real_escape_string($connection, trim($_POST['data_nascimento']));
-    $senha = isset($_POST['senha']) ? mysqli_real_escape_string($connection, trim($_POST['senha'])) :'';
+    $senha = isset($_POST['senha']) ? mysqli_real_escape_string($connection, password_hash(trim($_POST['senha']), PASSWORD_DEFAULT)) :'';
     
     $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
  
-    mysqli_query($connection, $sql); 
+    mysqli_query($connection, $sql);
+
+    password_verify($senha, $hash);
 
 }
 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once("../config/database.php");
 require_once("../vendor/autoload.php");
 require_once("../app/Router.php");
